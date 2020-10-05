fecda94d84c1f680640c24103d67fb14b6e02962
nirsky <nir@aspecto.io>
2020-10-05 15:48:23 +0300

    json response

--- src/index.ts
+++ src/index.ts
@@ -18,9 +18,9 @@ const app = express()
     .use(express.json())
     .use(express.urlencoded({ extended: true }));
 
-app.get('/test', async (req, res) => {
+app.get('/test', async (_req, res) => {
     await new Promise((r) => setTimeout(r, Math.random() * 80));
-    res.status(200).send(crypto.randomBytes(Math.random() * 10000).toString('hex'));
+    res.status(200).send({ randomBytes: crypto.randomBytes(Math.random() * 10000).toString('hex') });
 });
 
 app.listen(PORT, () => logger.info(`Server started on port ${PORT}`));
