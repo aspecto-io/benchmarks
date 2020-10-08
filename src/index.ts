3bab34f62a7e0057d1144f5542620fad6c0fbebe
nirsky <nir@aspecto.io>
2020-10-08 13:33:55 +0300

    process memory and cpu collecting

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { PORT, ASPECTO, OTEL } from './config';
 import logger from '@aspecto/logger';
 import Instrument from '@aspecto/opentelemetry';
 import { initOtel } from './opentelemetry';
+import pidusage from 'pidusage';
 
 if (ASPECTO) {
     logger.info(`Instrumenting using Aspecto process at port ${PORT}`);
@@ -20,6 +21,57 @@ const app = express()
 app.get('/test', async (_req, res) => {
     await new Promise((r) => setTimeout(r, 30));
     res.status(200).send({ randomBytes: 'hello world!' });
+});
+
+interface ProcessInfo {
+    cpu: number;
+    memory: number;
+}
+
+const processInfo: ProcessInfo[] = [];
+let collect = false;
+
+const toFixedNumber = (num: number) => Math.round(num * 1e2) / 1e2;
+
+const collectStatsWithInterval = async (time: number) => {
+    setTimeout(async () => {
+        const stats = await pidusage(process.pid);
+        const entry = {
+            cpu: toFixedNumber(stats.cpu),
+            memory: toFixedNumber(stats.memory / (1024 * 1024)),
+        };
+        processInfo.push(entry);
+        collect && collectStatsWithInterval(time);
+    }, time);
+};
+
+app.get('/start-collecting', async (_req, res) => {
+    collect = true;
+    collectStatsWithInterval(500);
+    res.send({ message: `Stating to collect memory and CPU for process ${process.pid}` });
+});
+
+const average = (array: number[]) => (array.length ? (array.reduce((a, b) => a + b) / array.length).toFixed(2) : null);
+
+app.get('/report', async (_req, res) => {
+    collect = false;
+    const cpus = processInfo.map((info) => info.cpu);
+    const cpu = {
+        description: 'CPU% Usage',
+        min: Math.min(...cpus),
+        max: Math.max(...cpus),
+        avg: average(cpus),
+    };
+    const memories = processInfo.map((info) => info.memory);
+    const memory = {
+        description: 'Memory in MB Used',
+        min: Math.min(...memories),
+        max: Math.max(...memories),
+        avg: average(memories),
+    };
+
+    res.send({ cpu, memory });
+    processInfo.length = 0;
 });
 
 app.listen(PORT, () => logger.info(`Server started on port ${PORT}`));
