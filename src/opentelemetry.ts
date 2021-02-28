5a50c36697f12b87707b6f8d860bd7f8b9dac3fa
Nir Hadassi <nir@aspecto.io>
2021-02-28 18:32:57 +0200

    fix dependencies (#53)

--- src/opentelemetry.ts
+++ src/opentelemetry.ts
@@ -1,25 +1,44 @@
 import { ZIPKIN } from './config';
 import { NodeTracerProvider } from '@opentelemetry/node';
+import { diag, DiagLogLevel } from '@opentelemetry/api';
 import { BatchSpanProcessor } from '@opentelemetry/tracing';
 import { CollectorTraceExporter } from '@opentelemetry/exporter-collector';
 import { ZipkinExporter } from '@opentelemetry/exporter-zipkin';
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
+import { ExpressInstrumentation } from '@aspecto/opentelemetry-instrumentation-express';
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
 
 const addZipkinExporter = (provider: NodeTracerProvider) => {
     provider.addSpanProcessor(
         new BatchSpanProcessor(
-            new ZipkinExporter({ url: 'https://jaeger-collector.aspecto.io:443/api/v2/spans', serviceName: 'benchmarks' })
+            new ZipkinExporter({
+                url: 'https://jaeger-collector.aspecto.io:443/api/v2/spans',
+                serviceName: 'benchmarks',
+            })
         )
     );
 };
 
 export const initOtel = () => {
-    const provider = new NodeTracerProvider({
-        plugins: {
-            express: {
-                enabled: true,
-                path: '@opentelemetry/plugin-express',
+    diag.setLogLevel(DiagLogLevel.NONE);
+    const provider = new NodeTracerProvider();
+    registerInstrumentations({
+        tracerProvider: provider,
+        instrumentations: [
+            new ExpressInstrumentation(),
+            new HttpInstrumentation(),
+            {
+                plugins: {
+                    // disabled plugins
+                    dns: { enabled: false },
+                    http: { enabled: false },
+                    https: { enabled: false },
+                    express: { enabled: false },
+                    pg: { enabled: false },
+                    'pg-pool': { enabled: false },
+                },
             },
-        },
+        ],
     });
 
     provider.addSpanProcessor(
